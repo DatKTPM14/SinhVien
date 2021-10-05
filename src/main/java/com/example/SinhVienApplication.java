6c24fce7e8e285ced74f312629b65ef8899ff3da
dat nguyen <thedatnguyen20@gmail.com>
2021-10-05 16:45:42 +0700

    eureka

--- src/main/java/com/example/SinhVienApplication.java
+++ src/main/java/com/example/SinhVienApplication.java
@@ -2,10 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SinhVienApplication {
 
 	public static void main(String[] args) {
