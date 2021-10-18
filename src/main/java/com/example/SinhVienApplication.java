7a11cf3d890b646f118b061b45b950f0184f0fbf
dat nguyen <thedatnguyen20@gmail.com>
2021-10-18 11:35:44 +0700

    retry

--- src/main/java/com/example/SinhVienApplication.java
+++ src/main/java/com/example/SinhVienApplication.java
@@ -2,20 +2,21 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class SinhVienApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SinhVienApplication.class, args);
 	}
 	@Bean
-    //@LoadBalanced
+//    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
--- src/main/java/com/example/service/SinhVienService.java
+++ src/main/java/com/example/service/SinhVienService.java
@@ -4,9 +4,13 @@ import com.example.VO.Khoa;
 import com.example.VO.ResponseTemplateVO;
 import com.example.entity.SinhVien;
 import com.example.repository.SinhVienRepository;
+import io.github.resilience4j.retry.annotation.Retry;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 @Service
@@ -20,17 +24,22 @@ public class SinhVienService {
     public SinhVien saveSinhVien(SinhVien sinhVien) {
         return sinhVienRepository.save(sinhVien);
     }
+    @Retry(name="basic")
     public ResponseTemplateVO getUserWithKhoa(Long sinhVienId) {
         ResponseTemplateVO vo = new ResponseTemplateVO();
         SinhVien sinhVien = sinhVienRepository.findById(sinhVienId).get();
         vo.setSinhVien(sinhVien);
         Khoa khoa =
-                restTemplate.getForObject("http://localhost:8080/khoa/"
+                restTemplate.getForObject("http://localhost:9001/khoa/"
                                 + sinhVien.getKhoaId(),
                         Khoa.class);
 
         vo.setKhoa(khoa);
 
         return vo;
+    }
+    public ResponseEntity<String> orderFallback(Exception e){
+        return
+                new ResponseEntity<String>("Item service is down!", HttpStatus.OK);
     }
 }
